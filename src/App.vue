10c407e58611a3387d8004b1949ceab83fcf42ce
Batuhan Sena Sert <batuhansenasert@gmail.com>
2024-06-27 19:04:52 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const count: number = ref(0);
+</script>
+
+<template>
+  <main>
+    <div>
+      <h4>The current count is...</h4>
+      <h1>{{ count }}</h1>
+      <button @click="count--">-</button>
+      <button @click="count = 0">Reset</button>
+      <button @click="count++">+</button>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+main {
+  background-color: aliceblue;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+div {
+  text-align: center;
+}
+</style>
